6c93cb049da595c4b84367f22826a9ec696d1dd0
Gabriel Alves Oliveira <51445468+prgoliveira@users.noreply.github.com>
2022-07-04 22:21:10 -0300

    incrementing beuatifulsoup4 and handling exceptions
    Changing the example for library usage and handling exceptions

--- python-scraping/ch01/exemplos/exemplo01.py
+++ python-scraping/ch01/exemplos/exemplo01.py
@@ -1,6 +1,16 @@
 from cgitb import html
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
 
-html = urlopen('https://pythonscraping.com/pages/page1.html')
 
-print(html.read())
+try:
+    html = urlopen('https://pythonscraping.com/pages/page1.html')
+except HTTPError as e:
+    print(e)
+else:
+    print('It Worked')
+
+bs = BeautifulSoup(html.read(), 'html5lib')
+
+print(bs)
