109b3e52beb486797454291fa64e677e11fb2d73
Gabriel Alves Oliveira <51445468+prgoliveira@users.noreply.github.com>
2022-07-20 12:59:05 -0300

    generating map from wikipedia
    created function that maps the links without repetition of the wikipedia site

---
+++ python-scraping/ch03/exemplos/exemplo4.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen('https://en.wikipedia.org{}'.format(pageUrl))
+    bs = BeautifulSoup(html.read(), 'html5lib')
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+
+
+getLinks('')
